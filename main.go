20ff2d22b92ae7c2871990f0a9ba9b9a5c18c91e
Dan Barry <dan@bakineggs.com>
2012-12-28 17:38:02 -0600

    use Basic technique (even though it doesn't do anything yet)

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 import "./bonsai/interpreter"
+import "./bonsai/interpreter/techniques"
 
 func main() {
 	rules := CompiledRules()
-	techniques := []bonsai.Technique{}
+	techniques := []bonsai.Technique{&techniques.Basic{}}
 
 	for _, technique := range techniques {
 		technique.Learn(rules)
